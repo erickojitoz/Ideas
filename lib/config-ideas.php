07b31c43dc5d144198e3cba6261d50a43123ce57
Daniel Ruiz <druiz7@gmail.com>
2013-07-05 17:01:18 +0200

    DONE: Add own analitycs

--- lib/config-ideas.php
+++ lib/config-ideas.php
@@ -1,4 +1,32 @@
 <div class="wrap srp">
     <h2>Ideas' Template Configuration</h2>
     <br class="clear">
+
+    <?php
+		if(get_option('ideas_analitycs')){
+			$ideas_analitycs = get_option('ideas_analitycs');
+		}else{
+			add_option( 'ideas_analitycs', '', '', 'yes' );
+		}
+		if(isset($_POST["analitycs"])){
+			$analitycs = $_POST["analitycs"];
+			update_option('ideas_analitycs',$analitycs);
+		}
+		else if(get_option('ideas_analitycs')){
+			$analitycs = get_option('ideas_analitycs');
+		}
+		else{
+			add_option( 'ideas_analitycs', '', '', 'yes' );
+		}
+		$ideas_analitycs = get_option('ideas_analitycs');
+	?>
+	<div class="wrap srp">
+		<h3>Google Analitycs</h3>
+		<br class="clear">
+
+		<form action="#" method='post'>
+			<input type="text" name="analitycs" value="<?php echo $ideas_analitycs; ?>" size="50">
+			<input type="submit" value="Submit">
+		</form>
+	</div>
 </div>

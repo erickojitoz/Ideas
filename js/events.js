1225b0b99ba787588ad0d4e2bc0be8d7baf801bf
Victor Tejada Yau <victortyau@gmail.com>
2013-07-09 00:42:47 -0500

    almost done the task about social network

--- js/events.js
+++ js/events.js
@@ -10,6 +10,13 @@ function social_networks_events(){
 		var netfour = jQuery("#Ideas_social_network_four_choose").prop("selectedIndex");
 		var netfive = jQuery("#Ideas_social_network_five_choose").prop("selectedIndex");
 		var indexes = new Array(netone,nettwo,netthree,netfour,netfive);
+		var linkone = jQuery("#Ideas_social_network_one").val();
+		var linktwo = jQuery("#Ideas_social_network_two").val();
+		var linkthree = jQuery("#Ideas_social_network_three").val();
+		var linkfour = jQuery("#Ideas_social_network_four").val();
+		var linkfive = jQuery("#Ideas_social_network_five").val();
+		var links = new Array(linkone,linkone,linkthree,linkfour,linkfive);
+		
 		var validate = new Array('','','','','','','','','','','','');
 		var error_show = "";
 		var error_message = new Array('',
@@ -25,41 +32,91 @@ function social_networks_events(){
 		'Instagran cannot be selected more than once',
 		'Github cannot be selected more than once');
 		
+		var error_link_message = new Array('',
+		"Facebook link doesn't match ",
+		"Twitter link doesn't match ",
+		"SoundCloud link doesn't match ",
+		"Google Plus link doesn't match ",
+		"Skype link doesn't match ",
+		"Linkedin link doesn't match ",
+		"Email link doesn't match ",
+		"Youtube link doesn't match ",
+		"Pinterest link doesn't match ",
+		"Instagran link doesn't match ",
+		"Github link doesn't match "
+		);
+		
+		var skype_regexp = /[a-zA-Z][a-zA-Z0-9\.,\-_]{5,31}/; 
+		var email_regexp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		
 		validate[0] = "";
 		for(var i = 0; i < indexes.length; i++){
 			switch(indexes[i]){
 				case 1:
 					 validate[1] += "0";
+					 if( links[i].indexOf("facebook.com/") < 0  ){
+						error_show += error_link_message[1];
+					 }
 					break
 				case 2:
 					validate[2] += "0";
+					if( links[i].indexOf("twitter.com/") < 0  ){
+						error_show += error_link_message[2];
+					 }
 					break;
 				case 3:
 					validate[3] += "0";
+					if( links[i].indexOf("soundcloud.com/") < 0  ){
+						error_show += error_link_message[3];
+					 }
 					break;
 				case 4:
 					validate[4] += "0";
+					if( links[i].indexOf("plus.google.com/") < 0  ){
+						error_show += error_link_message[4];
+					 }
 					break;
 				case 5:
 					validate[5] += "0";
+					if(skype_regexp.test(links[i]) == false){
+						error_show += error_link_message[5];
+					}
 					break;
 				case 6:
 					validate[6] += "0";
+					if( links[i].indexOf("linkedin.com") < 0  ){
+						error_show += error_link_message[6];
+					}
 					break;
 				case 7:
 					validate[7] += "0";
+					if( email_regexp.test(links[i]) == false ){
+						error_show += error_link_message[7];
+					}
 					break;
 				case 8:
 					validate[8] += "0";
+					if( links[i].indexOf("www.youtube.com/") < 0  ){
+						error_show += error_link_message[8];
+					}
 					break;
 				case 9:
 					validate[9] += "0";
+					if( links[i].indexOf("pinterest.com/") < 0  ){
+						error_show += error_link_message[9];
+					}
 					break;
 				case 10:
 					validate[10] += "0";
+					if( links[i].indexOf("instagram.com/") < 0  ){
+						error_show += error_link_message[10];
+					}
 					break;
 				case 11:
 					validate[11] += "0";
+					if( links[i].indexOf("github.com/") < 0  ){
+						error_show += error_link_message[11];
+					}
 					break;
 			}
 		}
@@ -74,7 +131,7 @@ function social_networks_events(){
 			alert(error_show);
 		}
 		else{
-			jQuery("#Ideas_social_network_form").submit();
+			jQuery("#Ideas_social_network_form").submit;
 		}
 	});
 }

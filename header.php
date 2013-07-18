7dca08fd08ae0a039198f40abc0c65c2fa2b057b
Erick Rubio <erick.rubio@live.com>
2013-07-18 00:47:53 -0500

    Before Meeting
    Premaster antes de la junta de maniana

--- header.php
+++ header.php
@@ -19,23 +19,12 @@
   <meta charset='utf-8'>
   <title><?php bloginfo('name'); ?> | <?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
   <meta name="viewport" content="width = device-width; initial-scale = 1.0; maximum-scale = 1.0; user-scalable = no">
-  <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/img/icon.ico">
  	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" media="screen">
   <link href='http://fonts.googleapis.com/css?family=Poiret+One|Roboto:300' rel='stylesheet' type='text/css'>
   <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js"></script>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
   <script src="<?php bloginfo('template_directory') ?>/js/main.js"></script>
   <script src="<?php bloginfo('template_directory') ?>/js/events.js"></script>
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-28061144-13', 'ideaspublcity.com');
-    ga('send', 'pageview');
-
-  </script>
   <script type="text/javascript">
     $(function () {
       $(".rslides").responsiveSlides({
